d6a6118a2d78ee3dd8858925d37f3a778fb4c032
pietoner <36419897+pietoner@users.noreply.github.com>
2018-09-29 22:20:22 +0300

    Add files via upload

---
+++ drone.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+
+cap=cv2.VideoCapture(0)
+
+while(True):
+    ret, frame=cap.read()
+
+    gray=cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2GRAY)
+
+    blurred_frame = cv2.GaussianBlur(frame,(5,5),0)
+
+    
+
+    circles=cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 10)
+
+    if circles is not None and len(circles)==1:
+        print(circles)
+        x=circles[0][0]
+        y=circles[0][1]
+        r=circles[0][2]
+        
+cap.release()
+
+cv2.destroyAllWindows()
+    
+    
+    
+    
+
+
+
+
+
